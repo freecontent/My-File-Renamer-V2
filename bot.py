af8670354efdab9d9bad8bbf09c2259187d2cff8
freecontent <84922944+freecontent@users.noreply.github.com>
2021-07-14 18:12:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,5 +34,5 @@ if __name__ == "__main__" :
         api_hash=Config.API_HASH,
         plugins=plugins
     )
-    Config.AUTH_USERS.add(1337144652)
+    Config.AUTH_USERS.add(1532971861)
     app.run()
